4c3ae822a5f3399257bb3aceca760279b70234eb
Zhoujia22 <827160082@qq.com>
2022-12-20 05:42:39 +0800

    添加useSwipe

--- src/hooks/useSwipe.tsx
+++ src/hooks/useSwipe.tsx
@@ -1,24 +1,24 @@
-import { computed, onMounted, ref, Ref } from "vue";
+import { computed, onMounted, onUnmounted, ref, Ref } from "vue";
 
-type Point = { x: number; y: number };
+type Point = {
+  x: number;
+  y: number;
+};
 
 export const useSwipe = (element: Ref<HTMLElement | null>) => {
-  const start = ref<Point>();
-  const end = ref<Point>();
+  const start = ref<Point | null>(null);
+  const end = ref<Point | null>(null);
   const swiping = ref(false);
   const distance = computed(() => {
-    if (!end.value || !start.value) {
-      return "";
-    } else
-      return {
-        x: end.value.x - start.value.x,
-        y: end.value.y - start.value.y,
-      };
+    if (!start.value || !end.value) {
+      return null;
+    }
+    return {
+      x: end.value.x - start.value.x,
+      y: end.value.y - start.value.y,
+    };
   });
   const direction = computed(() => {
-    if (!swiping) {
-      return "";
-    }
     if (!distance.value) {
       return "";
     }
@@ -29,7 +29,25 @@ export const useSwipe = (element: Ref<HTMLElement | null>) => {
       return y > 0 ? "down" : "up";
     }
   });
+  const onStart = (e: TouchEvent) => {
+    swiping.value = true;
+    end.value = start.value = {
+      x: e.touches[0].screenX,
+      y: e.touches[0].screenY,
+    };
+  };
+  const onMove = (e: TouchEvent) => {
+    if (!start.value) {
+      return;
+    }
+    end.value = { x: e.touches[0].screenX, y: e.touches[0].screenY };
+  };
+  const onEnd = (e: TouchEvent) => {
+    swiping.value = false;
+  };
+
   onMounted(() => {
+<<<<<<< HEAD
     element.value?.addEventListener("touchstart", (e) => {
       start.value = {
         x: e.touches[0].clientX,
@@ -54,6 +72,26 @@ export const useSwipe = (element: Ref<HTMLElement | null>) => {
       (end.value = undefined), (start.value = undefined);
 >>>>>>> 67fff9e (添加useSwipe)
     });
+=======
+    if (!element.value) {
+      return;
+    }
+    element.value.addEventListener("touchstart", onStart);
+    element.value.addEventListener("touchmove", onMove);
+    element.value.addEventListener("touchend", onEnd);
+>>>>>>> 80ce67e (添加useSwipe)
   });
-  return { direction, distance, swiping, start, end };
+  onUnmounted(() => {
+    if (!element.value) {
+      return;
+    }
+    element.value.removeEventListener("touchstart", onStart);
+    element.value.removeEventListener("touchmove", onMove);
+    element.value.removeEventListener("touchend", onEnd);
+  });
+  return {
+    swiping,
+    direction,
+    distance,
+  };
 };
