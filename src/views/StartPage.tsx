83cbcaaa113f3202e160c0b2e58b663c193b7d9c
Zhoujia22 <827160082@qq.com>
2022-12-24 03:06:54 +0800

    创建FloatButton组件和Icon组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,11 +1,20 @@
 import { defineComponent } from "vue";
 import { Button } from "../shared/Button";
+import { FloatButton } from "../shared/FloatButton";
 import s from "./StartPage.module.scss";
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const onClick = () => {
+      console.log("hi");
+    };
     return () => (
-      <div class={s.button_wrapper}>
-        <Button class={s.button}>测试</Button>;
+      <div>
+        <div class={s.button_wrapper}>
+          <Button class={s.button} onClick={onClick}>
+            测试
+          </Button>
+        </div>
+        <FloatButton />
       </div>
     );
   },
