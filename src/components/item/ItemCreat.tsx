0f2df329047276918707a4fe5ff4daf60264ce9e
Zhoujia22 <827160082@qq.com>
2022-12-25 20:29:37 +0800

    wip:Tabs组件

--- src/components/item/ItemCreat.tsx
+++ src/components/item/ItemCreat.tsx
@@ -1,6 +1,7 @@
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
+import { Tabs, Tab } from "../../shared/Tabs";
 import s from "./ItemCreat.module.scss";
 export const ItemCreat = defineComponent({
   props: {
@@ -9,6 +10,7 @@ export const ItemCreat = defineComponent({
     },
   },
   setup: (props, context) => {
+    const refKind = ref("支出");
     return () => (
       <MainLayout>
         {{
@@ -16,7 +18,10 @@ export const ItemCreat = defineComponent({
           icon: () => <Icon name="left" class={s.navIcon} />,
           default: () => (
             <>
-              <div>main</div>
+              <Tabs v-model:selected={refKind.value}>
+                <Tab name="支出">icon 列表</Tab>
+                <Tab name="收入">icon 列表</Tab>
+              </Tabs>
             </>
           ),
         }}
