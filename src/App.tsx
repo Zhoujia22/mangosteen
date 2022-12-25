46a37e1028c757d7c1cab4a5e9153c6de58132da
Zhoujia22 <827160082@qq.com>
2022-12-26 03:34:10 +0800

    wip:制作数字按键

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { defineComponent, Transition, VNode } from "vue";
-import { RouteLocationNormalizedLoaded, RouterView } from "vue-router";
-import "./App.scss"
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import "./App.scss";
 
 export const App = defineComponent({
   setup() {
