059982c8581bbec983f1903ed1b6064df6acd562
Zhoujia22 <827160082@qq.com>
2022-12-26 06:01:17 +0800

    使用vant 时间选择器

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -2,6 +2,7 @@ import { defineComponent, PropType, ref } from "vue";
 import { Icon } from "../../shared/Icon";
 import { time } from "../../shared/time";
 import s from "./InputPad.module.scss";
+import { Button, DatetimePicker, Popup } from "vant";
 
 export const InputPad = defineComponent({
   props: {
@@ -10,8 +11,11 @@ export const InputPad = defineComponent({
     },
   },
   setup: (props, context) => {
-    const refDtate = ref<Date>();
     const now = new Date();
+    const refDate = ref<Date>(now);
+    const refDatePickerVisible = ref(false);
+    const showDatePicker = () => (refDatePickerVisible.value = true);
+    const hideDatePicker = () => (refDatePickerVisible.value = false);
     const buttons = [
       { text: "1", onClick: () => {} },
       { text: "2", onClick: () => {} },
@@ -36,7 +40,24 @@ export const InputPad = defineComponent({
           <span class={s.date}>
             <Icon name="date" class={s.icon} />
             <span>
-              <input type="date" value={time(now).format()} />
+              <span onClick={showDatePicker}>
+                {time(refDate.value).format()}
+              </span>
+              <Popup
+                position="bottom"
+                v-model:show={refDatePickerVisible.value}
+              >
+                <DatetimePicker
+                  value={refDate.value}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={(date: Date) => {
+                    refDate.value = date;
+                    hideDatePicker();
+                  }}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
             </span>
           </span>
           <span class={s.amount}>123.12</span>
