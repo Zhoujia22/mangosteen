2d584f1dfdb452049421191e65ba2c9a79ea8355
Zhoujia22 <827160082@qq.com>
2023-01-19 18:24:09 +0800

    合并 /start 和 /items

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -6,10 +6,13 @@ import {
   ref,
   watch,
 } from 'vue';
+import { RouterLink } from 'vue-router';
 import { Button } from '../../shared/Button';
+import { Center } from '../../shared/Center';
 import { Datetime } from '../../shared/Datetime';
 import { FloatButton } from '../../shared/FloatButton';
 import { http } from '../../shared/Http';
+import { Icon } from '../../shared/Icon';
 import { Money } from '../../shared/Money';
 import s from './ItemSummary.module.scss';
 export const ItemSummary = defineComponent({
@@ -86,7 +89,7 @@ export const ItemSummary = defineComponent({
     onMounted(fetchItemsBalance);
     return () => (
       <div class={s.wrapper}>
-        {items.value ? (
+        {items.value && items.value.length > 0 ? (
           <>
             <ul class={s.total}>
               <li>
@@ -131,9 +134,20 @@ export const ItemSummary = defineComponent({
             </div>
           </>
         ) : (
-          <div>记录为空</div>
+          <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+          </>
         )}
-        <FloatButton iconName="add" />
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
       </div>
     );
   },
