1266281918c060a8b5db5bc9d7254ea859865e25
Zhoujia22 <827160082@qq.com>
2023-01-22 18:53:29 +0800

    tags_id 改为 tag_ids

--- src/components/tag/TagForm.tsx
+++ src/components/tag/TagForm.tsx
@@ -17,9 +17,9 @@ export const TagForm = defineComponent({
       id: undefined,
       name: '',
       sign: '😀',
-      kind: route.query.kind!.toString(),
+      kind: route.query.kind!.toString() as 'expenses' | 'income',
     });
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
+    const errors = reactive<FormErrors<typeof formData>>({});
 
     onMounted(async () => {
       if (!props.id) {
